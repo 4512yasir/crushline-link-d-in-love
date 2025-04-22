1478132aa43d4c22dbbdd048ab68316e4ef97abd
4512yasir <yasiryaska1@gmail.com>
2025-04-22 16:56:05 +0300

    general theme

---
+++ src/Themes/Theme.js
@@ -0,0 +1,9 @@
+export const themes = {
+    general: {
+      background: "#fdf6f0",
+      primary: "#ff4b5c",
+      secondary: "#1e2022",
+      accent: "#ffb400",
+      font: "'Poppins', sans-serif",
+    },
+  };
