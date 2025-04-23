683d92fb6ca9eb4d95af72eed927957d1b214b34
4512yasir <yasiryaska1@gmail.com>
2025-04-23 09:47:05 +0300

    resolved issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
 import React from "react";
-import MalePage from "./Components/MalePage"; 
 import "./App.css";
-import SignUp from "./Components/SignUp";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MalePage />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
+   <>
+   
+   </>
   );
 }
 
