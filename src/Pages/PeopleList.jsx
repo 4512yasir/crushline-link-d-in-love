8d39de34ab7d05540a0f96a67c52eab39d55772d
4512yasir <yasiryaska1@gmail.com>
2025-04-24 21:39:40 +0300

    changes

--- src/Pages/PeopleList.jsx
+++ src/Pages/PeopleList.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import PersonCard from "../Components/PersonCard";
 import useFetch from "../useFetch";
 import '../Css/peopleList.css';
-import Header from "../Components/Header";
+
 
 export default function PeopleList() {
   const { data: users, loading, error } = useFetch("http://localhost:3000/user");
@@ -22,7 +22,7 @@ export default function PeopleList() {
 
   return (
     <>
-      <Header />
+     
       <div className="matches"><h1>Matches</h1></div>
       <div className="list">
         {users.map((user) => (
