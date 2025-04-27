d38b50bdb7f781043182a85fc2bba96ea21fac32
4512yasir <yasiryaska1@gmail.com>
2025-04-27 07:34:54 +0300

    preparing for deployment

--- server.js
+++ server.js
@@ -6,19 +6,20 @@ import jsonServer from 'json-server';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Setup json-server
+// Setup json-server (mock API)
 const apiRouter = jsonServer.router('db1.json');
 const middlewares = jsonServer.defaults();
 app.use('/api', middlewares, apiRouter);
 
-// Serve React static files
-app.use(express.static(path.join(process.cwd(), 'build')));
+// Serve Vite static files
+app.use(express.static(path.join(process.cwd(), 'dist')));
 
 // Handle React routing, return all requests to React app
 app.get('*', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'build', 'index.html'));
+  res.sendFile(path.join(process.cwd(), 'dist', 'index.html'));
 });
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
